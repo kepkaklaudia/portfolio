6da557761eb39292f947ec4694ef62e17664137e
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-29 15:35:58 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Projects from './pages/Projects';
+import Header from './common/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BrowserRouter>
+      <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 

67ae165c5e4c3ff4eff427e916b885b1aec1b15d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-29 16:05:39 +0100

    themes added

---
+++ src/themes/theme.js
@@ -0,0 +1,137 @@
+import TealK from "./icon/TealK.svg"
+import MintK from "./icon/MintK.svg"
+import VeriPeriK from "./icon/VeriPeriK.svg"
+import OrangeK from "./icon/OrangeK.svg"
+import MagentaK from "./icon/MagentaK.svg"
+
+export const teal = {
+  name: "teal",
+  icon: TealK,
+  colors: {
+    bg: "dark",
+    variant: "dark",
+    text: "#FFF",
+    bodyBackground: "#020024",
+    styledNavLink: "#12B0B3",
+    styledNavLinkHover: "#FFF",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(326deg, rgba(0,255,244,1) 0%, rgba(0,199,190,1) 22%, rgba(255,0,228,1) 88%)",
+    subtitle: "#12B0B3",
+    icon: "#12B0B3",
+    iconHover: "#FFF",
+    cardText: "#000",
+    buttonBackground: "#020024",
+    buttonHover: "#2A257D",
+    accordionHeader: "#FFF"
+  },
+};
+
+export const mint = {
+  name: "mint",
+  icon: MintK,
+  colors: {
+    bg: "light",
+    variant: "light",
+    text: "#000",
+    bodyBackground: "#D6FFCC",
+    styledNavLink: "#00B576",
+    styledNavLinkHover: "#000",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(130deg, rgba(0,148,97,1) 0%, rgba(0,42,27,1) 49%, rgba(0,24,16,1) 100%)",
+    subtitle: "#002A1B",
+    icon: "#00B576",
+    iconHover: "#002A1B",
+    cardText: "#000",
+    buttonBackground: "#002A1B",
+    buttonHover: "#00B576",
+    accordionHeader: "#FFF"
+  },
+};
+
+export const blue = {
+  name: "blue",
+  icon: VeriPeriK,
+  colors: {
+    bg: "dark",
+    variant: "dark",
+    text: "#FFF",
+    bodyBackground: "#020024",
+    styledNavLink: "#BFCEFF",
+    styledNavLinkHover: "#FFF",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(130deg, rgba(54,45,218,1) 14%, rgba(104,118,255,1) 54%, rgba(245,247,255,1) 100%)",
+    subtitle: "#BFCEFF",
+    icon: "#BFCEFF",
+    iconHover: "#FFF",
+    cardText: "#000",
+    buttonBackground: "#020024",
+    buttonHover: "#2A257D",
+    accordionHeader: "#FFF"
+  },
+};
+
+export const orange = {
+  name: "orange",
+  icon: OrangeK,
+  colors: {
+    bg: "dark",
+    variant: "dark",
+    text: "#FFF",
+    bodyBackground: "#020024",
+    styledNavLink: "#FF8000",
+    styledNavLinkHover: "#FFF",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(146deg, rgba(255,34,0,1) 0%, rgba(255,98,0,1) 44%, rgba(255,209,0,1) 100%)",
+    subtitle: "#FF8000",
+    icon: "#FF8000",
+    iconHover: "#FFF",
+    cardText: "#000",
+    buttonBackground: "#020024",
+    buttonHover: "#2A257D",
+    accordionHeader: "#FFF"
+  },
+};
+
+export const magenta = {
+  name: "magenta",
+  icon: MagentaK,
+  colors: {
+    bg: "light",
+    variant: "light",
+    text: "#000",
+    bodyBackground: "#FFDEE6",
+    styledNavLink: "#CC0E3B",
+    styledNavLinkHover: "#000",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(130deg, rgba(143,22,51,1) 0%, rgba(94,7,28,1) 49%, rgba(41,0,10,1) 100%)",
+    subtitle: "#40111C",
+    icon: "#C93659",
+    iconHover: "#40111C",
+    cardText: "#000",
+    buttonBackground: "#40111C",
+    buttonHover: "#C93659",
+    accordionHeader: "#FFF"
+  },
+};
+
+export const turquoise = {
+  name: "turquoise",
+  icon: TealK,
+  colors: {
+    bg: "dark",
+    variant: "dark",
+    text: "#FFF",
+    bodyBackground: "#020024",
+    styledNavLink: "#0CF2E3",
+    styledNavLinkHover: "#FFF",
+    styledNavBrandHover: "#000",
+    headingGradient: "-webkit-linear-gradient(130deg, rgba(0,125,190,1) 7%, rgba(0,207,142,1) 63%, rgba(0,217,127,1) 100%)",
+    subtitle: "#0CF2E3",
+    icon: "#0CF2E3",
+    iconHover: "#FFF",
+    cardText: "#000",
+    buttonBackground: "#020024",
+    buttonHover: "#2A257D",
+    accordionHeader: "#FFF"
+  },
+};

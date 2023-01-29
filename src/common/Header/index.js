574346aaa8547d4c9e304a585e51be9ced6355d8
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-29 16:56:19 +0100

    Navbar image added

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,24 +1,25 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { StyledNavLink, StyledNavBrand, StyledNavbar, StyledNavToggle } from "./styled";
+import { StyledNavLink, StyledNavBrand, StyledNavbar, StyledNavToggle, Image } from "./styled";
 
 const Header = () => {
   return (
-      <StyledNavbar as={Navbar} collapseOnSelect >
-        <Container>
-          <StyledNavBrand as={Navbar.Brand} href="/">
-          </StyledNavBrand>
-          <StyledNavToggle as={Navbar.Toggle} />
-          <Navbar.Collapse className="justify-content-end">
-            <Nav className="ml-auto">
-              <Nav.Link as={StyledNavLink} eventKey="1"  to="/">Home</Nav.Link>
-              <Nav.Link as={StyledNavLink} eventKey="2" to="/projects">Projects</Nav.Link>
-              <Nav.Link as={StyledNavLink} eventKey="3" to="/about">About</Nav.Link>
-              <Nav.Link as={StyledNavLink} eventKey="4" to="/gallery">Gallery</Nav.Link>
-              <Nav.Link as={StyledNavLink} eventKey="5"to="/contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </StyledNavbar>
+    <StyledNavbar as={Navbar} collapseOnSelect >
+      <Container>
+        <StyledNavBrand as={Navbar.Brand} href="/">
+          <Image />{' '} Klaudia Kępka
+        </StyledNavBrand>
+        <StyledNavToggle as={Navbar.Toggle} />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="ml-auto">
+            <Nav.Link as={StyledNavLink} eventKey="1" to="/">Home</Nav.Link>
+            <Nav.Link as={StyledNavLink} eventKey="2" to="/projects">Projects</Nav.Link>
+            <Nav.Link as={StyledNavLink} eventKey="3" to="/about">About</Nav.Link>
+            <Nav.Link as={StyledNavLink} eventKey="4" to="/gallery">Gallery</Nav.Link>
+            <Nav.Link as={StyledNavLink} eventKey="5" to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </StyledNavbar>
   );
 }
 

16f942981be48e7239c56cd23b64208674beb270
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-31 00:12:28 +0100

    getRepos movet to fetchRepos

--- src/pages/Projects/getRepos.js
+++
@@ -1,9 +0,0 @@
-export const getRepos = async () => {
-  const response = await fetch("https://api.github.com/users/kepkaklaudia/repos");
-  
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-
-  return await response.json();
-};

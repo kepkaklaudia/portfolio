fc9adf69e7e7c158230676417b0689b2909b7836
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 15:15:01 +0100

    visual improvements

--- src/pages/Projects/Motion.js
+++ src/pages/Projects/Motion.js
@@ -1,15 +1,21 @@
 import { motion } from "framer-motion";
 
 export const Motion = ({ animatedElement }) => {
-  const initial = { y: 100, opacity: 0 };
-  const animate = { y: 0, opacity: 1 };
+  const initial = {
+    y: 100,
+    opacity: 0
+  };
+  const animate = {
+    y: 0,
+    opacity: 1
+  };
 
-return (
-  <motion.div
-    initial={initial}
-    animate={animate}
-  >
-    {animatedElement}
-  </motion.div>
-);
+  return (
+    <motion.div
+      initial={initial}
+      animate={animate}
+    >
+      {animatedElement}
+    </motion.div>
+  );
 }

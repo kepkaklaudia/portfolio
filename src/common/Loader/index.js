c729da45942b2a2d1c4fdf4d4a1e3136533e4f45
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 16:14:36 +0100

    refactoring

--- src/common/Loader/index.js
+++ src/common/Loader/index.js
@@ -7,11 +7,24 @@ export const Loader = () => {
     <>
       <Row>
         <Col>
-          <Card as={StyledCard}>
+          <Card
+            as={StyledCard}
+          >
             <Card.Body>
-              <Card.Title>Loading...</Card.Title>
-              <Card.Text style={{ height: "150px" }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 500 500">
+              <Card.Title>
+                Loading...
+              </Card.Title>
+              <Card.Text
+                style={{
+                  height: "150px"
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  viewBox="0 0 500 500"
+                >
                   <MotionPath1 />
                   <MotionPath2 />
                 </svg>

13c16674f5ef85033b294ae9c787d320cd7897dd
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 16:12:44 +0100

    refactoring

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -3,19 +3,60 @@ import { StyledNavLink, StyledNavBrand, StyledNavbar, StyledNavToggle, Image } f
 
 const Header = () => {
   return (
-    <StyledNavbar as={Navbar} collapseOnSelect >
+    <StyledNavbar
+      as={Navbar}
+      collapseOnSelect
+    >
       <Container>
-        <StyledNavBrand as={Navbar.Brand} href="https://kepkaklaudia.github.io/portfolio/">
+        <StyledNavBrand
+          as={Navbar.Brand}
+          href="https://kepkaklaudia.github.io/portfolio/"
+        >
           <Image />{' '} Klaudia Kępka
         </StyledNavBrand>
-        <StyledNavToggle as={Navbar.Toggle} />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="ml-auto">
-            <Nav.Link as={StyledNavLink} eventKey="1" to="/">Home</Nav.Link>
-            <Nav.Link as={StyledNavLink} eventKey="2" to="/projects">Projects</Nav.Link>
-            <Nav.Link as={StyledNavLink} eventKey="3" to="/about">About</Nav.Link>
-            <Nav.Link as={StyledNavLink} eventKey="4" to="/gallery">Gallery</Nav.Link>
-            <Nav.Link as={StyledNavLink} eventKey="5" to="/contact">Contact</Nav.Link>
+        <StyledNavToggle
+          as={Navbar.Toggle}
+        />
+        <Navbar.Collapse
+          className="justify-content-end"
+        >
+          <Nav
+            className="ml-auto"
+          >
+            <Nav.Link
+              as={StyledNavLink}
+              eventKey="1"
+              to="/"
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={StyledNavLink}
+              eventKey="2"
+              to="/projects"
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              as={StyledNavLink}
+              eventKey="3"
+              to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={StyledNavLink}
+              eventKey="4"
+              to="/gallery"
+            >
+              Gallery
+            </Nav.Link>
+            <Nav.Link
+              as={StyledNavLink}
+              eventKey="5"
+              to="/contact"
+            >
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

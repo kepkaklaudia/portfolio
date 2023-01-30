b120fbc9ee339d733e918c1891e272e9612eb211
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 23:38:46 +0100

    ImageBlocks added

--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,19 +1,8 @@
-import { Col, Image, Row } from "react-bootstrap";
-import France from "./images/France.jpg";
-import Milano from "./images/Milano.jpg";
-import Spain from "./images/Spain.jpg";
-import Italy from "./images/Italy.jpg";
-import France1 from "./images/France1.jpg";
-import Portugal from "./images/Portugal.jpg";
-import Spain2 from "./images/Spain2.jpg";
-import Spain3 from "./images/Spain3.jpg";
-import Spain4 from "./images/Spain4.jpg";
-import Cracow from "./images/Cracow.jpg";
-import LaSalette from "./images/LaSalette.jpg";
-import Germany from "./images/Germany.jpg";
+import { Col, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { Loader } from "../../common/Loader";
 import { Motion } from "./Motion";
+import { ImageBlock1, ImageBlock2, ImageBlock3 } from "./ImageBlocks";
 
 const Gallery = () => {
   const [status, setStatus] = useState("loading");
@@ -48,76 +37,19 @@ const Gallery = () => {
               md={4}
               className="mb-4 mb-lg-0"
             >
-              <Image
-                src={France}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="France"
-              />
-              <Image
-                src={Milano}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Milano"
-              />
-              <Image
-                src={Spain}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Spain"
-              />
-              <Image
-                src={Italy}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Italy"
-              />
+              <ImageBlock1 />
             </Col>
             <Col
               md={4}
               className="mb-4 mb-lg-0"
             >
-              <Image
-                src={France1}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="France1"
-              />
-              <Image
-                src={Portugal}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Portugal"
-              />
-              <Image
-                src={Spain2}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Spain2"
-              />
-              <Image
-                src={Spain3}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Spain3"
-              />
+              <ImageBlock2 />
             </Col>
             <Col
               md={4}
               className="mb-4 mb-lg-0"
             >
-              <Image
-                src={Spain4}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Spain4"
-              />
-              <Image
-                src={Cracow}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Cracow"
-              />
-              <Image
-                src={LaSalette}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="LaSalette"
-              />
-              <Image
-                src={Germany}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt="Germany"
-              />
+              <ImageBlock3 />
             </Col>
           </Row>
         </>

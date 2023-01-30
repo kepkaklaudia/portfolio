e94b350c911f1d08b90e0af4c916e60a6db158ff
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 01:04:02 +0100

    List animation added

--- src/pages/Contact/ContactList.js
+++ src/pages/Contact/ContactList.js
@@ -1,29 +1,68 @@
 import { StyledItem, StyledLink, StyledList } from "./styled";
+import { motion } from "framer-motion";
 
 export const ContactList = () => {
+  const initial = {
+    opacity: 0,
+    y: -500
+  };
+  const animate = {
+    opacity: 1,
+    y: 0
+  };
+  const transition = {
+    type: "spring",
+    stiffness: 50
+  };
+  const whileHover = {
+    scale: 1.1,
+    originX: 0
+  };
+
   return (
     <>
       <StyledList>
         <StyledItem>
-          <StyledLink
-            target="_blank"
-            rel="noopener noreferrer"
-            href="mailto:kepkaklaudia@gmail.com"
+          <motion.li
+            initial={initial}
+            animate={animate}
+            transition={transition}
+            whileHover={whileHover}
           >
-            📩 kepkaklaudia@gmail.com
-          </StyledLink>
+            <StyledLink
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:kepkaklaudia@gmail.com"
+            >
+              📩 kepkaklaudia@gmail.com
+            </StyledLink>
+          </motion.li>
         </StyledItem>
         <StyledItem>
-          📲 515-943-229
+          <motion.li
+            initial={initial}
+            animate={animate}
+            transition={transition}
+            whileHover={whileHover}
+          >
+            📲 515-943-229
+          </motion.li>
         </StyledItem>
         <StyledItem>
-          <StyledLink
-            target="_blank"
-            rel="noopener noreferrer"
-            href="github.com/kepkaklaudia"
+          <motion.li
+            initial={initial}
+            animate={animate}
+            transition={transition}
+            whileHover={whileHover}
           >
-            🌐 github.com/kepkaklaudia
-          </StyledLink>
+            <StyledLink
+              target="_blank"
+              rel="noopener noreferrer"
+              href="github.com/kepkaklaudia"
+            >
+              🌐 github.com/kepkaklaudia
+            </StyledLink>
+          </motion.li>
         </StyledItem>
       </StyledList>
     </>

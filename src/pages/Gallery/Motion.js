5db1c4390b5e478fcf1989602951c26ad8475db7
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 14:13:48 +0100

    Motion added

---
+++ src/pages/Gallery/Motion.js
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+
+export const Motion = ({animatedElement}) =>{
+  const initial = {
+    opacity: 0,
+    y: -100
+  };
+  const animate = {
+    opacity: 1,
+    y: 0
+  };
+
+  return (
+    <motion.div
+      initial={initial}
+      animate={animate}
+    >
+      {animatedElement}
+    </motion.div>
+  );
+};

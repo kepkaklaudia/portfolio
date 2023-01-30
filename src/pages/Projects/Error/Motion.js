d0e330dde4e9d226e88f5bd4f010592401e557eb
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-30 16:13:52 +0100

    MotionLines created

---
+++ src/pages/Projects/Error/Motion.js
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+
+const initial = {
+  pathLength: 0,
+  opacity: 0
+};
+const animate = {
+  pathLength: 1,
+  opacity: 1
+};
+const transition = (i) => {
+  return {
+    opacity: 1,
+    duration: 1,
+    delay: 1 + 0.5 * i,
+  }
+};
+
+export const MotionLine1 = () => {
+  return (
+    <motion.line
+      initial={initial}
+      animate={animate}
+      transition={transition(0)}
+      stroke="#990000"
+      strokeWidth="8px"
+      strokeLinecap="round"
+      x1="0"
+      y1="0"
+      x2="70"
+      y2="70"
+    />
+  );
+};
+
+export const MotionLine2 = () => {
+  return (
+    <motion.line
+      initial={initial}
+      animate={animate}
+      transition={transition(1)}
+      stroke="#990000"
+      strokeWidth="8px"
+      strokeLinecap="round"
+      x1="0"
+      y1="70"
+      x2="70"
+      y2="0"
+    />
+  );
+};
+
+

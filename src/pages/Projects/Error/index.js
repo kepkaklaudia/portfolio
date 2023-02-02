4fdb0a8846d6bd5f579eaa512f96cfc578ca4df2
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-02 23:35:03 +0100

    Text added

--- src/pages/Projects/Error/index.js
+++ src/pages/Projects/Error/index.js
@@ -1,43 +1,40 @@
 import { Row, Col, Card } from "react-bootstrap";
 import { StyledCard, StyledButton } from "../styled";
 import { MotionLine1, MotionLine2 } from "./Motion";
+import { Text } from "./styled";
 
-export const Error = () => {
-  return (
-    <>
-      <Row>
-        <Col>
-          <Card
-            as={StyledCard}
-          >
-            <Card.Body>
-              <Card.Title>
-                Oops! Error loading...
-              </Card.Title>
-              <Card.Text
-                style={{
-                  height: "150px"
-                }}
+export const Error = () => (
+  <>
+    <Row>
+      <Col>
+        <Card
+          as={StyledCard}
+        >
+          <Card.Body>
+            <Card.Title>
+              Oops! Error loading...
+            </Card.Title>
+            <Card.Text 
+              as={Text}
+            >
+              Press Open GitHub to check the projects directly on GitHub!
+              <svg
+                viewBox="-50 -25 125 125"
+                width="80%"
+                height="80%"
               >
-                Press Open GitHub to check the projects directly on GitHub!
-                <svg
-                  viewBox="-50 -25 125 125"
-                  width="80%"
-                  height="80%"
-                >
-                  <MotionLine1 />
-                  <MotionLine2 />
-                </svg>
-              </Card.Text>
-              <StyledButton
-                href="https://github.com/kepkaklaudia?tab=repositories"
-              >
-                Open Github
-              </StyledButton>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  );
-}
+                <MotionLine1 />
+                <MotionLine2 />
+              </svg>
+            </Card.Text>
+            <StyledButton
+              href="https://github.com/kepkaklaudia?tab=repositories"
+            >
+              Open Github
+            </StyledButton>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </>
+);

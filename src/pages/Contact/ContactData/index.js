c03bb29356ee8c58640d6d3103f2f094e2895dc0
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-02 23:16:52 +0100

    refactoring

--- src/pages/Contact/ContactData/index.js
+++ src/pages/Contact/ContactData/index.js
@@ -2,36 +2,39 @@ import { StyledItem, StyledList } from "./styled";
 import { Link } from "./Link";
 import { Motion } from "./Motion";
 
-export const ContactData = () => {
-  return (
-    <>
-      <StyledList>
-        <StyledItem>
-          <Motion
-            animatedElement={
-              <Link
-                href={"mailto:kepkaklaudia@gmail.com"}
-                description={"📩 kepkaklaudia@gmail.com"}
-              />
-            }
-          />
-        </StyledItem>
-        <StyledItem>
-          <Motion
-            animatedElement={"📲 515-943-229"}
-          />
-        </StyledItem>
-        <StyledItem>
-          <Motion
-            animatedElement={
-              <Link
-                href={"https://github.com/kepkaklaudia"}
-                description={"🌐 github.com/kepkaklaudia"}
-              />
-            }
-          />
-        </StyledItem>
-      </StyledList>
-    </>
-  );
-};
+export const ContactData = () => (
+  <>
+    <StyledList>
+      <StyledItem>
+        <Motion
+          animatedElement={
+            <Link
+              href={"mailto:kepkaklaudia@gmail.com"}
+              description={"📩 kepkaklaudia@gmail.com"}
+            />
+          }
+        />
+      </StyledItem>
+      <StyledItem>
+        <Motion
+          animatedElement={
+            <Link
+              href={"tel:+48515943229"}
+              description={"📲 515-943-229"}
+            />
+          }
+        />
+      </StyledItem>
+      <StyledItem>
+        <Motion
+          animatedElement={
+            <Link
+              href={"https://github.com/kepkaklaudia"}
+              description={"🌐 github.com/kepkaklaudia"}
+            />
+          }
+        />
+      </StyledItem>
+    </StyledList>
+  </>
+);
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,64 +4,62 @@ import Extra from "./images/Extra.png"
 import { ContactData } from "./ContactData";
 import { Motion } from "./Motion";
 
-const Contact = () => {
-  return (
-    <Container>
-      <Row
-        className="align-items-center justify-content-center text-center"
+const Contact = () => (
+  <Container>
+    <Row
+      className="align-items-center justify-content-center text-center"
+    >
+      <Col
+        xs={{
+          span: 7,
+          order: "last"
+        }}
+        sm={{
+          span: 5,
+          order: "first"
+        }}
+        md={4}
+        lg={3}
       >
-        <Col
-          xs={{
-            span: 7,
-            order: "last"
-          }}
-          sm={{
-            span: 5,
-            order: "first"
-          }}
-          md={4}
-          lg={3}
-        >
-          <Motion
-            x={-300}
-            type={"spring"}
-            animatedElement={
-              <img
-                alt="Contact"
-                className="w-100"
-                src={Extra} />
-            }
-          />
-        </Col>
-        <Col
-          xs={{
-            span: 12,
-            order: "first"
-          }}
-          sm={{
-            span: 7,
-            order: "last"
-          }}
-          md={8}
-          lg={8}
-        >
-          <Motion
-            y={-300}
-            duration={1}
-            animatedElement={
-              <>
-                <Title>
-                  Let's create something <strong>together!</strong>
-                </Title>
-                We can talk in Italian and English, i will understand your Spanish, but to enter the creative world we can communicate in JavaScript in the React dialect.
-              </>
-            }
-          />
-          <ContactData />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+        <Motion
+          x={-300}
+          type={"spring"}
+          animatedElement={
+            <img
+              alt="Contact"
+              className="w-100"
+              src={Extra} />
+          }
+        />
+      </Col>
+      <Col
+        xs={{
+          span: 12,
+          order: "first"
+        }}
+        sm={{
+          span: 7,
+          order: "last"
+        }}
+        md={8}
+        lg={8}
+      >
+        <Motion
+          y={-300}
+          duration={1}
+          animatedElement={
+            <>
+              <Title>
+                Let's create something <strong>together!</strong>
+              </Title>
+              We can talk in Italian and English, i will understand your Spanish, but to enter the creative world we can communicate in JavaScript in the React dialect.
+            </>
+          }
+        />
+        <ContactData />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Contact;

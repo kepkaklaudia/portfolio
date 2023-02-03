4730e4261c48d83d365057977b249c6e68ae067f
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-03 14:44:34 +0100

    headings added

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -5,11 +5,17 @@ import { ReposCards } from "./ReposCards";
 const Projects = () => (
   <Motion animatedElement={
     <>
+      <h2>
+        Technologies & Tools i use
+      </h2>
       <div
         className="d-flex justify-content-center flex-wrap align-items-start"
       >
         <IconsBlock />
       </div>
+      <h2>
+        My projects
+      </h2>
       <ReposCards />
     </>
   } />

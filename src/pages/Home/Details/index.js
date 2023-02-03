d06623df24fb7ad3ad570477848d397410651c3c
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-03 16:13:13 +0100

    refactoring

--- src/pages/Home/Details/index.js
+++ src/pages/Home/Details/index.js
@@ -3,58 +3,86 @@ import { Col } from "react-bootstrap";
 import { Subtitle } from "../styled";
 import { Motion } from "./Motion";
 
-export const Details = () => (
-  <>
-    <Col xs={12} className="mt-5 mb-4 w-75">
-      <Motion x={-200} animatedElement={
+export const Details = () => {
+  const elements = [
+    {
+      animatedElement1:
         <>
-          <Subtitle>Projects </Subtitle>
+          <Subtitle>
+            Projects
+          </Subtitle>
           Here you will find projects I have worked on and a list of technologies and tools that I use every day and which I am still learning.
-        </>
-      } />
-    </Col>
-    <Col xs={12} className="my-2">
-      <Motion x={200} animatedElement={
-        <StyledLink to="/projects"> See my Projects! </StyledLink>
-      } />
-    </Col>
-    <Col xs={12} className="mt-5 mb-4 w-75">
-      <Motion x={-200} animatedElement={
+        </>,
+      animatedElement2:
+        <StyledLink to="/projects">
+          See my Projects!
+        </StyledLink>
+    },
+    {
+      animatedElement1:
         <>
-          <Subtitle>About</Subtitle>
+          <Subtitle>
+            About
+          </Subtitle>
           Read the short story of my life!
-        </>
-      } />
-    </Col>
-    <Col xs={12} className="my-2">
-      <Motion x={200} animatedElement={
-        <StyledLink to="/about" > Read about me! </StyledLink>
-      } />
-    </Col>
-    <Col xs={12} className="mt-5 mb-4 w-75">
-      <Motion x={-200} animatedElement={
+        </>,
+      animatedElement2:
+        <StyledLink to="/about">
+          Read about me!
+        </StyledLink>
+    },
+    {
+      animatedElement1:
         <>
-          <Subtitle>Gallery </Subtitle>
+          <Subtitle>
+            Gallery
+          </Subtitle>
           Get to know who I am through photos and see one of my greatest passions!
-        </>
-      } />
-    </Col>
-    <Col xs={12} className="my-2">
-      <Motion x={200} animatedElement={
-        <StyledLink to="/gallery" > Go to Gallery! </StyledLink>
-      } />
-    </Col>
-    <Col xs={12} className="mt-5 mb-4 w-75">
-      <Motion x={-200} animatedElement={
+        </>,
+      animatedElement2:
+        <StyledLink to="/gallery">
+          Go to Gallery!
+        </StyledLink>
+    },
+    {
+      animatedElement1:
         <>
-          <Subtitle>Contact</Subtitle>
+          <Subtitle>
+            Contact
+          </Subtitle>
           See my contact details.
-        </>} />
-    </Col>
-    <Col xs={12} className="mt-2 mb-4">
-      <Motion x={200} animatedElement={
-        <StyledLink to="/contact"> Let's get in touch! </StyledLink>
-      } />
-    </Col>
-  </>
-);
+        </>,
+      animatedElement2:
+        <StyledLink to="/contact">
+          Let's get in touch!
+        </StyledLink>
+    },
+  ];
+
+  return (
+    <>
+      {elements.map(element =>
+        <>
+          <Col
+            xs={12}
+            className="mt-5 mb-4 w-75"
+          >
+            <Motion
+              x={-200}
+              animatedElement={element.animatedElement1}
+            />
+          </Col>
+          <Col
+            xs={12}
+            className="my-2"
+          >
+            <Motion
+              x={200}
+              animatedElement={element.animatedElement2}
+            />
+          </Col>
+        </>
+      )}
+    </>
+  );
+};

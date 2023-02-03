a2cdf5de82a66f3bdef79541958d9180a8d8eed5
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-03 15:44:08 +0100

    Motion added

---
+++ src/pages/Home/Details/Motion.js
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+
+export const Motion = ({ x, animatedElement }) => {
+  const initial = {
+    x
+  };
+  const transition = {
+    duration: 0.7
+  };
+  const whileInView = {
+    x: 0
+  };
+  const viewport = {
+    once: true,
+    amount: 0.1
+  };
+
+  return (
+    <motion.div
+      initial={initial}
+      whileInView={whileInView}
+      transition={transition}
+      viewport={viewport}
+      x
+    >
+      {animatedElement}
+    </motion.div>
+  );
+};

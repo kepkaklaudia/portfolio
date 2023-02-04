f00ab9117e13722309385f17be7ebfb070f0b7c6
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-04 22:26:41 +0100

    scrollTo added

--- src/pages/Home/Chevron/index.js
+++ src/pages/Home/Chevron/index.js
@@ -1,22 +1,36 @@
 import { ChevronMotion } from "./Motion";
 import { Wrapper } from "./styled";
+import { useRef } from "react";
 
-export const Chevron = () => (
-  <>
-    <Wrapper>
-      <ChevronMotion animatedElement={
-        <svg 
-        xmlns="http://www.w3.org/2000/svg"
-        width="50" 
-        height="50" 
-        fill="currentColor" 
-        className="bi bi-chevron-down" 
-        viewBox="0 0 16 16">
-          <path 
-          fillRule="evenodd" 
-          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
-        </svg>
-      } />
-    </Wrapper>
-  </>
-);
+export const Chevron = () => {
+  const section = useRef(null);
+
+  const scrollTo = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth"
+    });
+  }
+
+  return (
+    <>
+      <Wrapper>
+        <ChevronMotion animatedElement={
+          <svg
+            onClick={() => scrollTo(section)}
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            className="bi bi-chevron-down"
+            viewBox="0 0 16 16">
+            <path
+              fillRule="evenodd"
+              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+          </svg>
+        } />
+      </Wrapper>
+      <div ref={section} />
+    </>
+  );
+}
